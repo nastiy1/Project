e57b90546bb36b16c9b37761bcafd6835880e452
N <nastiy1@live.ru>
2021-08-30 19:23:27 +0700

    Задача№1

---
+++ Lesson2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Постановка задачи: 1.Пользователь вводит мин. и мах. температуру за сутки; 2.Вывести среднесуточную температуру за сутки.
+
+            Console.WriteLine("Введите минимальную температуру:");
+            int mint = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Введите максимальную температуру:");
+            int maxt = Convert.ToInt16(Console.ReadLine());
+            int result = (mint + maxt) / 2;
+            Console.WriteLine($"Среднесуточная температура:{result}");
+        }
+    }
+}
