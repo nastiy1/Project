aea4b218fbb45c943c056a08cf8667d1a131c661
N <nastiy1@live.ru>
2021-09-01 21:01:17 +0700

    Задание №4

---
+++ Lesson2_4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lesson2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nomerticket = 005818;
+            long nomerseriy = 0294990088519013;
+            int nomermarshrut = 31;
+            string nomerbus = "P 697 CP";
+            double cost = 24.00;
+
+            Console.WriteLine("Добро пожаловать !");
+            Console.WriteLine("ООО ПАП-5");
+            Console.WriteLine($"Билет № {nomerticket}");
+            Console.WriteLine($"Серия {nomerseriy}");
+            Console.WriteLine(("15.08.2019 14:10"), DateTime.Today.GetDateTimeFormats());
+            Console.WriteLine($"Маршрут:№ {nomermarshrut}");
+            Console.WriteLine($"Автобус {nomerbus}");
+            Console.WriteLine($"Сумма: {cost} руб.");
+
+
+
+
+        }
+    }
+}
