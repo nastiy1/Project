fc0ec4edc11a9058f9b57463cb4fa6aff7d1b27c
N <nastiy1@live.ru>
2021-09-06 18:40:12 +0700

    Задача №2

--- Lesson3_2/Program.cs
+++ Lesson3_2/Program.cs
@@ -6,7 +6,21 @@ namespace Lesson3_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Написать программу — телефонный справочник — создать двумерный массив 5*2, хранящий список телефонных контактов: первый элемент хранит имя контакта, второй — номер телефона/e-mail.
+            string[,] phonebook = new string[5, 2];
+            for (int i = 0; i < phonebook.GetLength(0); i++)
+            {
+
+                Console.WriteLine("Введите имя контакта:");
+                string name = Console.ReadLine();
+                phonebook[i, 0] = name;
+                Console.WriteLine("Введите номер моб.телефона и адрес электронной почты:");
+                string numberphone = Console.ReadLine();
+                phonebook[i, 1] = numberphone;
+                
+
+
+            }
         }
     }
 }
