a81451daa25368caa77f3ed2a2ec30ce3ae27946
N <nastiy1@live.ru>
2021-09-09 09:01:08 +0700

    Задача№2

--- Lesson4_2/Program.cs
+++ Lesson4_2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Lesson4_2
 {
@@ -6,7 +7,21 @@ namespace Lesson4_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*Написать программу, принимающую на вход строку — набор чисел, разделенных пробелом,
+             * и возвращающую число — сумму всех чисел в строке. 
+             * Ввести данные с клавиатуры и вывести результат на экран.*/
+
+
+            int sum = SSum();
+
+            static int SSum()
+            {
+                Console.WriteLine("Введите числа:");
+                int[] numbers = Array.ConvertAll(Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries), (numbers) => Convert.ToInt32(numbers));
+                int result = numbers.Sum();
+                Console.WriteLine(result);
+                return result;
+            }
         }
     }
 }
