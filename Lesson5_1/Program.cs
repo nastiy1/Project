1202ebc3a7c3269c6071a1d9e5cca1b8ed9038c5
N <nastiy1@live.ru>
2021-09-13 19:01:30 +0700

    Create Program

---
+++ Lesson5_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+    */
+            string path = @"C:\Users\nasti\source\repos\Text.txt";
+            string createtext = Console.ReadLine();
+            StreamWriter sw = new StreamWriter(path, true);
+                sw.WriteLine(createtext);
+                sw.Close();
+            
+            StreamReader sr = new StreamReader(path);
+            Console.WriteLine(sr.ReadToEnd());
+        }
+    }
+}
