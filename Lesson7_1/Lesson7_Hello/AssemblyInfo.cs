a928f60b4e5d46afa3b1b9b736d32c36d2dda8ee
N <nastiy1@live.ru>
2021-09-20 15:27:49 +0700

    Create Program

---
+++ Lesson7_1/Lesson7_Hello/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Lesson7_1")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyProduct("Lesson7_1")]
+[assembly: AssemblyTitle("Lesson7_1")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ Lesson7_1/Lesson7_Hello/Program.cs
@@ -0,0 +1,22 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: Lesson7_1.Program
+// Assembly: Lesson7_1, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 0D15DF5C-7D25-4F3A-B312-78A79A121748
+// Assembly location: C:\Users\nasti\Progetgithub\Project\Lesson7_1\bin\Release\net5.0\Lesson7.exe
+
+using System;
+
+namespace Lesson7_1
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      string str = "2345";
+      Console.WriteLine("Enter password:");
+      if (Console.ReadLine() == str)
+        return;
+      Console.WriteLine("Welcome!");
+    }
+  }
+}
